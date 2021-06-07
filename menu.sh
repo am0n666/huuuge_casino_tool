9d14523d551af83563021dd61cbb81db221a6214
am0n666 <am0n666x@gmail.com>
2021-06-07 19:38:50 +0200

    xxx

--- menu.sh
+++ menu.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 
 function check_is_root() {
 	if [ "$(id -u)" -eq 0 ]
---
+++ reset.sh
@@ -0,0 +1,31 @@
+dir_list=(
+	_clb_/1/a
+	_clb_/1/b
+	_clb_/2/a
+	_clb_/2/b
+	_clb_/3/a
+	_clb_/3/b
+	_clb_/4/a
+	_clb_/5/a
+	_clb_/6/a
+	_clb_/7/a
+	_clb_/7/b
+	_clb_/8/a
+	_clb_/8/b
+	_clb_/9/a
+)
+
+curr="$PWD"
+
+for check_dir in ${dir_list[@]}
+do
+	if [ -n $check_dir ]
+	then
+#		if [ -d $check_dir/.info ]
+#		then
+#			rm -rf $check_dir/.info
+#		fi
+		echo 00 > $check_dir/.last_restored
+	fi
+done
+
