4d6931d5a4312c461330ece9d5e450f59ae5c92d
am0n666 <am0n666x@gmail.com>
2021-06-09 17:53:21 +0200

    xxx

--- clubs.sh
+++ clubs.sh
@@ -8,75 +8,69 @@ clubstmp="$homepwd/.clubs_temp"
 clubsupdateddir="$clubstmp/$reponame"
 
 function update_github() {
-	g_u_em=am0n666x@gmail.com
-	g_u_n=am0n666
-	g_p=1Oliwka1_
+    g_u_em=am0n666x@gmail.com
+    g_u_n=am0n666
+    g_p=1Oliwka1_
 
-	repo_name=$(basename "$PWD")
-	git init --quiet
-	git config user.email "$g_u_em"
-	git config user.name "$g_u_n"
-	git add -A
-	git commit -m "xxx" --quiet
-	git push https://$g_u_n:$g_p@github.com/$g_u_n/$repo_name.git --all --quiet
+    repo_name=$(basename "$PWD")
+    git init --quiet
+    git config user.email "$g_u_em"
+    git config user.name "$g_u_n"
+    git add -A
+    git commit -m "xxx" --quiet
+    git push https://$g_u_n:$g_p@github.com/$g_u_n/$repo_name.git --all --quiet
 }
 
 function upload_clubs() {
-	mkdir -p "$clubstmp"
-	cd $clubstmp
-	git clone --quiet --branch master $repo_url
-	cp -rf $clubsdir/* $clubsupdateddir/
-	cd $clubsupdateddir
-	update_github
-	cd $homepwd
-	rm -rf $clubstmp
+    mkdir -p "$clubstmp"
+    cd $clubstmp
+    git clone --quiet --branch master $repo_url
+    cp -rf $clubsdir/* $clubsupdateddir/
+    cd $clubsupdateddir
+    update_github
+    cd $homepwd
+    rm -rf $clubstmp
 }
 
 function download_clubs() {
-	if [ -d $clubsdir ]
-	then
-		rm -rf $clubsdir
-	fi
-	git clone $repo_url --branch master --quiet
-	if [ -d $clubsdir/.git ]
-	then
-		rm -rf $clubsdir/.git
-	fi
+    if [ -d $clubsdir ]; then
+        rm -rf $clubsdir
+    fi
+    git clone $repo_url --branch master --quiet
+    if [ -d $clubsdir/.git ]; then
+        rm -rf $clubsdir/.git
+    fi
 }
 
 function nout() {
-        "$@" > /dev/null 2>&1
+        "$@" >/dev/null  2>&1
 }
 
 function print_usage() {
-	echo "Usage:"
-	echo "clubs.sh download"
-	echo "clubs.sh upload"
-	echo "clubs.sh sync"
+    echo "Usage:"
+    echo "clubs.sh download"
+    echo "clubs.sh upload"
+    echo "clubs.sh sync"
 }
 
-if [ ! -n "$1" ]
-then
-	print_usage
-	exit
+if [ ! -n "$1" ]; then
+    print_usage
+    exit
 fi
 
 cmd="$1"
 
-if [ $cmd = "download" ]
-then
-	download_clubs
-elif [ $cmd = "upload" ]
-then
-	upload_clubs
-elif [ $cmd = "sync" ]
-then
-	echo ""
-	echo "Synchronization of clubs, please wait"
-	nout upload_clubs
-	nout download_clubs
-	echo "Done."
-	echo ""
+if [ $cmd = "download" ]; then
+    download_clubs
+elif [ $cmd = "upload" ]; then
+    upload_clubs
+elif [ $cmd = "sync" ]; then
+    echo ""
+    echo "Synchronization of clubs, please wait"
+    nout upload_clubs
+    nout download_clubs
+    echo "Done."
+    echo ""
 else
-	print_usage
+    print_usage
 fi
--- del_sh_ext.sh
+++ del_sh_ext.sh
@@ -1,3 +1,4 @@
+
 function get_from_path() {
     [[ $# != 2 ]] && echo "usage: get_from_path <path> <dir|name|fullname|ext>" && return
     theFullFilePath="$1"
@@ -16,3 +17,29 @@ function get_from_path() {
      ext) echo "${extension}"  ;;
     esac
 }
+
+function convert_path() {
+    _input="$1"
+    _output=""
+    _fch_=$(printf %.1s "$_input")
+    [ "$_fch_" = "/" ] && _output+="$_input"
+    [ ! "$_fch_" = "/" ] && _output+="$(pwd)/$_input"
+    _lch_=${_output:$((${#_output} - 1)):1}
+    [ "$_lch_" = "/" ] && _output="${_output:0:${#_output}-1}"
+    echo $_output
+}
+
+dirpath="$(convert_path "$1")"
+
+cd "$dirpath"
+
+for i in $(ls -d $dirpath/*); do
+
+mv "$dirpath/$(get_from_path "$i" fullname)" "$dirpath/$(get_from_path "$i" name)"
+#	sh_ext_fn="${i%%/}"
+#	without_ext_fn="$(get_from_path "$sh_ext_fn" name)"
+#	echo $sh_ext_fn $without_ext_fn
+done
+
+cd "$OLDPWD"
+
