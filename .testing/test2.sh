35d016ad0f89956d2279d711333bb0aea680f533
am0n666 <am0n666x@gmail.com>
2021-06-16 16:20:40 +0200

    xxx

---
+++ .testing/test2.sh
@@ -0,0 +1,64 @@
+		<node name="userSpecific">
+			<node name="collectibles">
+				<node name="3">
+					<node name="newItems">
+						<node name="1">
+							<key name="3" type="bool">true</key>
+						</node>
+						<node name="2">
+							<key name="11" type="bool">true</key>
+							<key name="16" type="bool">true</key>
+						</node>
+						<node name="3">
+							<key name="23" type="bool">true</key>
+							<key name="24" type="bool">true</key>
+							<key name="26" type="bool">true</key>
+						</node>
+						<node name="4">
+							<key name="37" type="bool">true</key>
+						</node>
+						<node name="5">
+							<key name="42" type="bool">true</key>
+							<key name="47" type="bool">true</key>
+						</node>
+						<node name="6">
+							<key name="52" type="bool">true</key>
+							<key name="53" type="bool">true</key>
+							<key name="54" type="bool">true</key>
+						</node>
+						<node name="7">
+							<key name="61" type="bool">true</key>
+							<key name="68" type="bool">true</key>
+						</node>
+						<node name="8">
+							<key name="74" type="bool">true</key>
+						</node>
+					</node>
+					<node name="newSets">
+						<key name="1" type="bool">true</key>
+						<key name="2" type="bool">true</key>
+						<key name="3" type="bool">true</key>
+						<key name="4" type="bool">true</key>
+						<key name="5" type="bool">true</key>
+						<key name="6" type="bool">true</key>
+						<key name="7" type="bool">true</key>
+						<key name="8" type="bool">true</key>
+					</node>
+				</node>
+				<node name="newThemes">
+					<key name="3" type="bool">true</key>
+				</node>
+				<node name="tutorial">
+					<node name="3">
+					</node>
+				</node>
+			</node>
+		</node>
+		<node name="vault">
+			<key name="info_popup_shown" type="bool">true</key>
+			<key name="tutorial_shown" type="bool">true</key>
+		</node>
+		<node name="workout">
+			<key name="static_tutorial_shown" type="bool">true</key>
+			<key name="tutorial_shown" type="bool">true</key>
+		</node>
